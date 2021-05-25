eaba78863e42199645c29391c388780e048f4ec9
VilgotL <54435285+VilgotL@users.noreply.github.com>
2021-05-25 10:58:03 +0200

    kommentarer

--- Template/Template/ItemListClass.cs
+++ Template/Template/ItemListClass.cs
@@ -9,8 +9,10 @@ namespace Template
 {
     static class ItemListClass
     {
+	//Itemlista
         public static List<Item> itemList = new List<Item>();
 
+	//Texturer för de olika itemsen
         public static Texture2D lifeTexture;
         public static Texture2D multiBulletTexture;
         public static Texture2D shieldTexture;
@@ -27,8 +29,12 @@ namespace Template
         //Tid det ska ta mellan item spawns
         public static float spawnTime = 8320f;
 
+	//Används för att generera ett slumpmässigt värde
         public static Random random = new Random();
 
+	///<summary>
+	///Används för att spawna ett item
+        ///</summary>
         public static void SpawnItem()
         {
             //Beräknar sannolikheten för en viss item spawn
@@ -42,18 +48,26 @@ namespace Template
                 itemList.Add(new Shield(shieldTexture, new Vector2(random.Next(0, 751), -50), new Rectangle(325, -50, 30, 30)));
         }
 
-        //Lägger till item i kön så länge det inte finns fler än 3 items redan i listan
+        ///<summary>
+	///Lägger till item i kön så länge det inte finns fler än 3 items redan i listan
+	///</summary>
         public static void AddItem(Item item)
         {
             if (itemQueue.Count < 3)
                 itemQueue.Enqueue(item);
         }
 
+	///<summary>
+	///Startar itemtimern
+	///</summary>
         public static void StartTimer()
         {
             spawnTimer.Start();
         }
 
+	///<summary>
+	///Startar om itemtimern
+	///</summary>
         public static void RestartTimer()
         {
             spawnTimer.Stop();
@@ -63,6 +77,7 @@ namespace Template
 
         public static void Update()
         {
+            //Spawnar item efter en viss tid
             if(spawnTimer.ElapsedMilliseconds > spawnTime)
             {
                 SpawnItem();
