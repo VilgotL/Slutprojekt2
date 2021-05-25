a81f7fa20a636922aee3f0fe43b217d6b6fd5990
VilgotL <54435285+VilgotL@users.noreply.github.com>
2021-05-25 10:45:20 +0200

    kommentarer

--- Template/Template/EnemyListClass.cs
+++ Template/Template/EnemyListClass.cs
@@ -9,8 +9,10 @@ namespace Template
 {
     static class EnemyListClass
     {
+	//Fiendelista
         public static List<EnemyClass> enemyList = new List<EnemyClass>();
 
+	//Textur för fiendena och kulorna i Enemy3s skottlista
         public static Texture2D enemyTexture;
         public static Texture2D bulletTexture;
 
@@ -20,8 +22,11 @@ namespace Template
         //Hur lång tid det ska ta att spawna en fiende
         public static float spawnTime = 2000;
 
+	//för att kunna få ett slumpmässigt värde
         public static Random random = new Random();
 
+	///<summary>
+	///Används för att spawna fiender
         private static void SpawnEnemy()
         {
             //Beräknar hur stor sannolikheten ska vara att en viss fiende ska spawna
@@ -37,11 +42,17 @@ namespace Template
 
         }
 
+	///<summary>
+        ///Startar fiendetimern
+	///</summary>
         public static void StartTimer()
         {
             spawnTimer.Start();
         }
 
+	///<summary>
+	///Startar om fiendetimern
+	///</summary>
         private static void RestartTimer()
         {
             spawnTimer.Stop();
@@ -49,7 +60,9 @@ namespace Template
             spawnTimer.Start();
         }
 
-        //ska komma fler fiender ju fler poäng man har
+        ///<summary>
+	///Minskar tiden som går mellan två fiendespawns
+	///</summary>
         public static void DecreaseSpawnTime()
 		{
             spawnTime = spawnTime * 0.98f;
@@ -57,6 +70,7 @@ namespace Template
 
         public static void Update()
         {
+	    //Spawnar fiende efter att tillräckligt lång tid har gått
             if (spawnTimer.ElapsedMilliseconds > spawnTime)
             {
                 SpawnEnemy();
