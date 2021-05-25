1e6562402bcbbc5c1b1a02e74283a4b064074fc2
VilgotL <54435285+VilgotL@users.noreply.github.com>
2021-05-25 10:53:36 +0200

    Kommentarer

--- Template/Template/Item.cs
+++ Template/Template/Item.cs
@@ -7,9 +7,16 @@ namespace Template
 {
     class Item: BaseClass
     {
+        //Hastighet för itemen
         protected float xSpeed = 0f;
         protected float ySpeed = 1.5f;
 
+        ///<summary>
+        ///Konstruktor för item
+        ///</summary>
+        ///<param name="texture">Texture för item</param>
+        ///<param name="position">Position för item</param>
+        ///<param name="rectangle">Hitbox för item</param>
         public Item(Texture2D texture, Vector2 position, Rectangle rectangle):base(texture, position, rectangle)
         {
             this.texture = texture;
@@ -17,8 +24,12 @@ namespace Template
             this.rectangle = rectangle;
         }
 
+        ///<summary>
+        ///Flyttar itemet
+        ///</summary>
         private void Move() 
         {
+            //hastigheten
             position.X += xSpeed;
             position.Y += ySpeed;
 
@@ -27,6 +38,9 @@ namespace Template
                 xSpeed = -xSpeed;
         }
 
+        ///<summary>
+        ///Tar bort Item från skärmen
+        ///</summary>
         public void Remove()
         {
             ySpeed = 0f;
@@ -35,9 +49,11 @@ namespace Template
             position = new Vector2(1200, 800);
         }
 
+        
         public override void Update()
         {
             Move();
+            //Flyttar hitboxen till rätt position
             rectangle.Location = position.ToPoint();
         }
 
