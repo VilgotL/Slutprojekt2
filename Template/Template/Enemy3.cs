b60ba70873b09521e8fd277179ea11469a018908
VilgotL <54435285+VilgotL@users.noreply.github.com>
2021-05-25 10:39:15 +0200

    kommentarer

--- Template/Template/Enemy3.cs
+++ Template/Template/Enemy3.cs
@@ -9,13 +9,22 @@ namespace Template
 {
     class Enemy3 : EnemyClass, IShootable
     {
+        //skottlista för Enemy3
         protected static List<Bullet> bulletList = new List<Bullet>();
 
+        //Textur för kulorna
         protected Texture2D bulletTexture;
 
         //Timer för att beräkna tiden sedan senaste skottet
         protected Stopwatch shootTimer = new Stopwatch();
 
+        ///<summary>
+        ///Konstruktor för Enemy3
+        ///</summary>
+        ///<param name="texture">Texture för fienden</param>
+        ///<param name="bulletTexture">Textur för kulorna</param>
+        ///<param name="position">Position för fienden</param>
+        ///<param name="rectangle">Hitbox för fienden</param>
         public Enemy3(Texture2D texture, Texture2D bulletTexture, Vector2 position, Rectangle rectangle) : base(texture, position, rectangle)
         {
             this.texture = texture;
@@ -23,18 +32,24 @@ namespace Template
             this.position = position;
             this.rectangle = rectangle;
 
+            //Hastighet och antal liv
             ySpeed = 1.5f;
             xSpeed = 0f;
             lives = 1;
 
+            //Startar skottimern
             shootTimer.Start();
         }
 
+        //Ger skottlistan
         public static List<Bullet> BulletList
         {
             get { return bulletList; }
         }
 
+        ///<summary>
+        ///Startar om skottimern
+        ///</summary>
         private void RestartTimer()
         {
             shootTimer.Stop();
@@ -42,6 +57,9 @@ namespace Template
             shootTimer.Start();
         }
 
+        ///<summary>
+        ///Gör så att fienden skjuter
+        ///</summary>
         public void Shoot()
         {
             //Skjuter om det har gått 1 sekund sedan förra skottet
