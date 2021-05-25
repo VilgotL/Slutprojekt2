fc89e42bcaf3781f7bab834ee067199865eb301a
VilgotL <54435285+VilgotL@users.noreply.github.com>
2021-05-25 10:32:27 +0200

    kommentarer

--- Template/Template/Enemy1.cs
+++ Template/Template/Enemy1.cs
@@ -7,12 +7,20 @@ namespace Template
 {
     class Enemy1 : EnemyClass
     {
+        ///<summary>
+        ///Konstruktor för Enemy1
+        ///</summary>
+        ///<param name="texture">Texture för fienden</param>
+        ///<param name="position">Position för fienden</param>
+        ///<param name="rectangle">Hitbox för fienden</param>
+        
         public Enemy1(Texture2D texture, Vector2 position, Rectangle rectangle) :base(texture, position, rectangle)
         {
             this.texture = texture;
             this.position = position;
             this.rectangle = rectangle;
 
+            //Hastighet och antal liv
             ySpeed = 1.5f;
             xSpeed = 0f;
             lives = 1;
